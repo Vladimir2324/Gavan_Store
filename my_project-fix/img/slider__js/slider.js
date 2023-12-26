43bacd528f9860e59aba62fdb967992e48dd1a5a
Vladimir2324 <114403871+Vladimir2324@users.noreply.github.com>
2023-12-26 13:28:19 +0300

    Add files via upload

---
+++ my_project-fix/img/slider__js/slider.js
@@ -0,0 +1,11 @@
+$('.single-item').slick();
+
+$(".autoplay").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000
+});
---
+++ my_project-fix/script.js
@@ -0,0 +1,12 @@
+$(()=>{
+  $('.single-item').slick();
+$(".multiple-items").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+})
+
+  
